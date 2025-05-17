5c8eaf37eeef319f1342434f91b4c251da106097
Max Blennemann <80426654+23maxb@users.noreply.github.com>
2025-05-17 12:03:26 -0700

    added file upload and reset

--- DjangoProject/models/views.py
+++ DjangoProject/models/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 import os
 from django.views.decorators.csrf import csrf_exempt
@@ -5,14 +7,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 def is_image_file(filename):
     image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'}
+    image_extensions = {'.png'}
     ext = os.path.splitext(filename)[1].lower()
     return ext in image_extensions
 
 
-def handle_uploaded_file(f, filename):
+def handle_uploaded_file(f, filename, id):
     downloads_dir = os.path.join('downloads')
     os.makedirs(downloads_dir, exist_ok=True)
-    file_path = os.path.join(downloads_dir, filename)
+    file_path = os.path.join(downloads_dir, str(id) + '_' + filename)
     with open(file_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
@@ -46,17 +49,36 @@ def hello(request):
         uploaded_file = request.FILES['file']
         filename = uploaded_file.name
         if not is_image_file(filename):
-            return HttpResponseBadRequest("Only image files are accepted.\n")
+            return HttpResponseBadRequest("Only png files are accepted.\n")
         if os.path.exists('downloads'):
             if filename in os.listdir('downloads'):
                 return HttpResponseBadRequest("File already exists.\n")
-        handle_uploaded_file(uploaded_file, uploaded_file.name)
         global upload_counter
         upload_counter += 1
+        handle_uploaded_file(uploaded_file, uploaded_file.name, upload_counter)
         return JsonResponse(
             {
                 "status": "success",
-                "id": upload_counter
+                "id": (upload_counter)
             }
         )
     return HttpResponseBadRequest("Only POST with a file is accepted.\n")
+
+
+def runModel(id):
+    print('hi')
+
+
+@csrf_exempt
+def predict(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        my_param = data.get('id')
+        return JsonResponse(
+            {
+                "status": "success",
+                "id": my_param,
+                "classification": runModel(my_param)
+            }
+        )
+    return JsonResponse({"status": "need post request"})
